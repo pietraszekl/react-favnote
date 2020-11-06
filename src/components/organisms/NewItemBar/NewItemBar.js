27f75ad574ae8313795b23469ca0f489daf1f9c1
Lukasz Pietraszek <lukasz.pietraszek@uk.kps.com>
2020-11-06 14:57:52 +0100

    redux actions

---
+++ src/components/organisms/NewItemBar/NewItemBar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Input from '../../atoms/Input/Input';
+import Button from '../../atoms/Button/Button';
+import withContext from '../../../hoc/withContext';
+import Heading from '../../atoms/Heading/Heading';
+import { addItem as addNoteAction } from '../../../actions/index';
+
+const StyledWrapper = styled.div`
+  border-left: 10px solid ${({ theme, activeColor }) => theme[activeColor]};
+  z-index: 9999;
+  position: fixed;
+  display: flex;
+  padding: 100px 90px;
+  flex-direction: column;
+  right: 0;
+  top: 0;
+  height: 100vh;
+  width: 680px;
+  background-color: white;
+  box-shadow: -5px 0 15px rgba(0, 0, 0, 0.1);
+  transform: translate(${({ isVisible }) => (isVisible ? '0' : '100%')});
+  transition: transform 0.25s ease-in-out;
+`;
+
+const StyledTextArea = styled(Input)`
+  border-radius: 10px;
+  border-radius: 20px;
+  height: 30vh;
+  margin-bottom: 20px;
+`;
+
+const StyledInput = styled(Input)`
+  margin-bottom: 30px;
+`;
+
+const NewItemBar = ({ pageContext, isVisible, addItem }) => (
+  <StyledWrapper isVisible={isVisible} activeColor={pageContext}>
+    <Heading big>Create new {pageContext}</Heading>
+    <StyledInput placeholder="title" />
+    {pageContext === 'articles' && <StyledInput placeholder="title" />}
+    <StyledTextArea as="textarea" placeholder="title" />
+    <Button
+      onClick={() =>
+        addItem(pageContext, {
+          title: 'Hello Lukasz',
+          content: 'lorem ipsum',
+        })
+      }
+      activecolor={pageContext}
+    >
+      Add note
+    </Button>
+  </StyledWrapper>
+);
+
+NewItemBar.propTypes = {
+  pageContext: PropTypes.oneOf(['notes', 'twitters', 'articles']),
+};
+
+NewItemBar.defaultProps = {
+  pageContext: 'notes',
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (itemType, itemContent) => dispatch(addNoteAction(itemType, itemContent)),
+});
+
+export default connect(null, mapDispatchToProps)(withContext(NewItemBar));
--- src/templates/GridTemplate.js
+++ src/templates/GridTemplate.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import UserPageTemplate from './UserPageTemplate';
@@ -6,8 +6,12 @@ import Input from '../components/atoms/Input/Input';
 import Heading from '../components/atoms/Heading/Heading';
 import Paragraph from '../components/atoms/Paragraph/Paragraph';
 import withContext from '../hoc/withContext';
+import ButtonIcon from '../components/atoms/ButtonIcon/ButtonIcon';
+import plusIcon from '../assets/icons/plus.svg';
+import NewItemBar from '../components/organisms/NewItemBar/NewItemBar';
 
 const StyledWrapper = styled.div`
+  position: relative;
   padding: 25px 150px 25px 70px;
 `;
 
@@ -34,20 +38,53 @@ const StyledParagraph = styled(Paragraph)`
   font-weight: ${({ theme }) => theme.bold};
 `;
 
-const GridTemplate = ({ children, pageContext }) => (
-  <UserPageTemplate>
-    <StyledWrapper>
-      <StyledPageHeader>
-        <Input search placeholder="Search" />
-        <StyledHeading big as="h1">
-          {pageContext}
-        </StyledHeading>
-        <StyledParagraph>6 {pageContext}</StyledParagraph>
-      </StyledPageHeader>
-      <StyledGrid>{children}</StyledGrid>
-    </StyledWrapper>
-  </UserPageTemplate>
-);
+const StyledButtonIcon = styled(ButtonIcon)`
+  background-color: ${({ activeColor, theme }) => theme[activeColor]};
+  background-size: 35%;
+  border-radius: 50px;
+  position: fixed;
+  bottom: 40px;
+  right: 40px;
+  z-index: 10000;
+`;
+
+class GridTemplate extends Component {
+  state = {
+    isNewItemBarVisible: false,
+  };
+
+  handleNewItemBarToggle = () => {
+    this.setState((prevState) => ({
+      isNewItemBarVisible: !prevState.isNewItemBarVisible,
+    }));
+  };
+
+  render() {
+    const { children, pageContext } = this.props;
+    const { isNewItemBarVisible } = this.state;
+
+    return (
+      <UserPageTemplate>
+        <StyledWrapper>
+          <StyledPageHeader>
+            <Input search placeholder="Search" />
+            <StyledHeading big as="h1">
+              {pageContext}
+            </StyledHeading>
+            <StyledParagraph>6 {pageContext}</StyledParagraph>
+          </StyledPageHeader>
+          <StyledGrid>{children}</StyledGrid>
+          <StyledButtonIcon
+            icon={plusIcon}
+            activeColor={pageContext}
+            onClick={this.handleNewItemBarToggle}
+          />
+          <NewItemBar isVisible={isNewItemBarVisible} />
+        </StyledWrapper>
+      </UserPageTemplate>
+    );
+  }
+}
 
 GridTemplate.propTypes = {
   children: PropTypes.arrayOf(PropTypes.object).isRequired,
